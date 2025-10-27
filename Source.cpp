05d868fd1aa60d12c152018a313f3e6dbd316cb7
phuongkhuu <thanhphuongkhuu@gmail.com>
2025-10-27 08:02:03 +0700

    Ham tong va tich

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
-
 int tong(int a, int b){
 	return a + b;
 }
+int hieu(int a, int b);
+int tich(int a, int b); {
+	return a * b;
+}
+float thuong(int a, int b);
+
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Ham tinh tong: " << tong(1,2);
+	cout << "Ham tinh tich: " << tich(4,6);
 	system("pause");
 	return 0;
 }
