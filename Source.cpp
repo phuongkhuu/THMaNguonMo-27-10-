0619e780589c06a0c974787d619a04d74237d133
TanKhaiThanh <chodichomoive@gmail.com>
2025-10-27 08:29:29 +0700

    Ham tinh tong, hieu va tich

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int tong(int a, int b){
 	return a + b;
 }
-int hieu(int a, int b);
+int hieu(int a, int b) {
+	return a - b;
+}
 int tich(int a, int b); {
 	return a * b;
 }
@@ -14,6 +16,7 @@ int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Ham tinh tong: " << tong(1,2);
+	cout << "Ham tinh hieu: " << hieu(5,3);
 	cout << "Ham tinh tich: " << tich(4,6);
 	system("pause");
 	return 0;
